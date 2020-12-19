a8f636df98e8433f145f198ddb5dddbb34256b2f
Vladimir Semenov <svowl05@gmail.com>
2020-12-19 11:23:33 +0400

    [*] Added DB cleanup

--- 20-lesson/1-schema.sql
+++ 20-lesson/1-schema.sql
@@ -1,3 +1,12 @@
+DROP TABLE IF EXISTS film_actors;
+DROP TABLE IF EXISTS film_directors;
+DROP TABLE IF EXISTS films;
+DROP TABLE IF EXISTS studios;
+DROP TABLE IF EXISTS directors;
+DROP TABLE IF EXISTS actors;
+
+DROP TYPE IF EXISTS pg_rating;
+
 -- actors - актеры
 CREATE TABLE actors (
     id SERIAL PRIMARY KEY,
@@ -5,6 +14,7 @@ CREATE TABLE actors (
     last_name TEXT NOT NULL DEFAULT '',
     year_of_birth INTEGER NOT NULL DEFAULT 0
 );
+CREATE INDEX IF NOT EXISTS actors_last_name_idx ON actors USING btree (lower(last_name));
 
 -- directors - режиссеры
 CREATE TABLE directors (
@@ -13,12 +23,14 @@ CREATE TABLE directors (
     last_name TEXT NOT NULL DEFAULT '',
     year_of_birth INTEGER NOT NULL DEFAULT 0
 );
+CREATE INDEX IF NOT EXISTS directors_last_name_idx ON directors USING btree (lower(last_name));
 
 -- studios - режиссеры
 CREATE TABLE studios (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL DEFAULT ''
 );
+CREATE INDEX IF NOT EXISTS name_idx ON studios USING btree (lower(name));
 
 -- тип для поля рейтинга
 CREATE TYPE pg_rating AS ENUM ('PG-10', 'PG-13', 'PG-18');
@@ -33,15 +45,16 @@ CREATE TABLE films (
     studio_id INTEGER REFERENCES studios(id) DEFAULT 0
 );
 -- уникальный индекс по названию фильма и году выхода
-CREATE UNIQUE INDEX IF NOT EXISTS title_year_idx ON films USING btree (title, year);
+CREATE UNIQUE INDEX IF NOT EXISTS title_year_idx ON films USING btree (lower(title), year);
 -- на всякий случай: индекс для title не создаем, потому что
 -- There's no need to manually create indexes on unique columns; doing so would just duplicate the automatically-created index.
 -- https://www.postgresql.org/docs/current/indexes-unique.html
+CREATE INDEX IF NOT EXISTS profit_idx ON films USING btree (profit);
+CREATE INDEX IF NOT EXISTS year_idx ON films USING btree (year);
 
 -- связь между фильмами и актерами
 -- (у одного фильма может быть несколько актеров)
 CREATE TABLE film_actors (
-    id BIGSERIAL PRIMARY KEY,
     film_id BIGINT NOT NULL REFERENCES films(id),
     actor_id INTEGER NOT NULL REFERENCES actors(id),
     UNIQUE(film_id, actor_id)
@@ -50,7 +63,6 @@ CREATE TABLE film_actors (
 -- связь между фильмами и режиссерами
 -- (у одного фильма может быть несколько режиссеров)
 CREATE TABLE film_directors (
-    id BIGSERIAL PRIMARY KEY,
     film_id BIGINT NOT NULL REFERENCES films(id),
     director_id INTEGER NOT NULL REFERENCES directors(id),
     UNIQUE(film_id, director_id)
